d5b47c4fbd6172c29e81dc7b32d339653a68ef50
Julia Marinho <marinhojulia25@hotmail.com>
2026-07-28 11:32:42 -0300

    Tela de listagem de produtos vendidos adicionada

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -166,7 +166,8 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-    
+        ListagemVendasView listagemVendas = new ListagemVendasView();
+        listagemVendas.setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
